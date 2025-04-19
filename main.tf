7f84774782588d1c2836aa7881976757db7e649b
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2025-04-19 16:02:51 +0200

    simpel cluster setup

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "hcloud_server" "juice_shop" {
 
     runcmd:
       - podman pull bkimminich/juice-shop
-      - podman run -d -p 3000:3000 --name juice-shop docker.io/bkimminich/juice-shop:v17.2.0
+      - podman run --restart -d -p 80:3000 --name juice-shop docker.io/bkimminich/juice-shop:v17.2.0
   EOT
 }
 
@@ -130,7 +130,7 @@ resource "hcloud_server" "bad_postgres" {
 resource "hcloud_server" "scb" {
   name        = "scb"
   image       = "ubuntu-24.04"
-  server_type = "cax11"
+  server_type = "cax31"
   location    = "fsn1"
 
   ssh_keys = [ "jannik-zuhause", "jannik-unterwegs" ]
@@ -138,7 +138,7 @@ resource "hcloud_server" "scb" {
   firewall_ids = [hcloud_firewall.block-incoming-internet.id]
 
   public_net {
-    ipv4_enabled = false
+    ipv4_enabled = true
     ipv6_enabled = true
   }
 
@@ -151,7 +151,44 @@ resource "hcloud_server" "scb" {
     #cloud-config
     package_update: true
     package_upgrade: true
-    packages:
-      - nmap
+
+    runcmd:
+      # 1. Install K3s
+      - curl -sfL https://get.k3s.io | sh -
+
+      # 2. Enable the K3s service
+      - systemctl enable k3s
+
+      # 3. Create the static manifests folder if it doesn't exist
+      - mkdir -p /var/lib/rancher/k3s/server/manifests
+
+      # 4. Restart K3s to ensure it loads from the static manifests
+      - systemctl restart k3s
+
+    write_files:
+      # Example Kubernetes Deployment YAML for static manifests
+      - path: /var/lib/rancher/k3s/server/manifests/sample-deployment.yaml
+        content: |
+          apiVersion: apps/v1
+          kind: Deployment
+          metadata:
+            name: nginx-deployment
+            labels:
+              app: nginx
+          spec:
+            replicas: 2
+            selector:
+              matchLabels:
+                app: nginx
+            template:
+              metadata:
+                labels:
+                  app: nginx
+              spec:
+                containers:
+                - name: nginx
+                  image: nginx:stable
+                  ports:
+                  - containerPort: 80
   EOT
 }
